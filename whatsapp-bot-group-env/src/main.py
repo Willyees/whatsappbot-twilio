9ba26a0bb269b06960729d1ce1f4b377cc965e30
Alessio Williams Gava <marveel@hotmail.it>
2021-07-07 11:17:24 +0100

    added business logic for automatic replying to messages

--- whatsapp-bot-group-env/src/main.py
+++ whatsapp-bot-group-env/src/main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
 
@@ -10,8 +11,19 @@ def root():
 
 @app.route("/bot", methods=["POST"])
 def botWebHook():
-    msg = request.values.get("Body", "")
-    return "bot"
+    msg = request.values.get("Body", "").lower()
+    # check msg contains all keywords when setting up meeting
+    keywords = ["tuesday", "sighthill", "power", "league"]
+    lst = [...]
+    if all((word in lst for word in keywords)):
+        resp = MessagingResponse()
+        body_resp = resp.message(
+            body="in"
+        )  # automatic response in case strings were found
+        return "message received and a response has been sent"
+
+    print("message received but a match could not be found")  # debug on console
+    return "message received but a match could not be found"  # debug on browser
 
 
 if __name__ == "__main__":
